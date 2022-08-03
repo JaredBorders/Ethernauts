cf3da04bf18b12bda1696ecd77a610268987daff
Jared Borders <jlbxsxs@gmail.com>
2022-08-03 16:15:49 -0500

    ✅ Hack Force

--- contracts/3. Force/Force.test.ts
+++ contracts/3. Force/Force.test.ts
@@ -5,18 +5,18 @@ let victim: any;
 let attacker: any;
 
 describe("Attacking Force", function () {
-  beforeEach(async () => {
-    const Victim = await ethers.getContractFactory("Force");
-    victim = await Victim.deploy();
-    const Attacker = await ethers.getContractFactory("AttackingForce");
-    attacker = await Attacker.deploy(victim.address, { value: 100 });
-  });
+    beforeEach(async () => {
+        const Victim = await ethers.getContractFactory("Force");
+        victim = await Victim.deploy();
+        const Attacker = await ethers.getContractFactory("AttackingForce");
+        attacker = await Attacker.deploy(victim.address, { value: 100 });
+    });
 
-  // Get this to pass!
-  it("Succesfully give the force contract some ETH", async () => {
-    await attacker.hackContract();
-    const provider = waffle.provider;
-    const balance = await provider.getBalance(victim.address);
-    expect(balance).to.be.above(0);
-  });
+    // Get this to pass!
+    it("Succesfully give the force contract some ETH", async () => {
+        await attacker.hackContract();
+        const provider = waffle.provider;
+        const balance = await provider.getBalance(victim.address);
+        expect(balance).to.be.above(0);
+    });
 });
